2798d9076356901a5b860c2d5f771954bc09b183
ethynic <ethynic@outlook.com>
2023-04-24 15:19:06 +0800

    修复session判断问题

--- app/login/view.py
+++ app/login/view.py
@@ -7,18 +7,20 @@ from ..models import User
 
 @login.route('/login',methods = ['GET', 'POST'])
 def login_in():
-
-    form = Form()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username = form.name.data).first()
-        if user is not None and user.vefiy_password(form.password.data):
-            login_user(user)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-               next = url_for('static',filename='index.html')
-               resp = make_response(redirect(next))
-               resp.set_cookie('user_id',str(user.id))
-               resp.set_cookie('user_name',user.username)
-            return resp
-        flash('用户名或者密码不正确')
-    return render_template('login.html',loginform=form)
+    if session:
+        return redirect(url_for('static',filename='index.html'))
+    else:
+        form = Form()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username = form.name.data).first()
+            if user is not None and user.vefiy_password(form.password.data):
+                login_user(user)
+                next = request.args.get('next')
+                if next is None or not next.startswith('/'):
+                    next = url_for('static',filename='index.html')
+                    resp = make_response(redirect(next))
+                    resp.set_cookie('user_id',str(user.id))
+                    resp.set_cookie('user_name',user.username)
+                    return resp
+            flash('用户名或者密码不正确')
+        return render_template('login.html',loginform=form)
